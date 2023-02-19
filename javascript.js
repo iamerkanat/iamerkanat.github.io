67425aafb732d8a56cd6ef695aab5bf0d170b459
Erkanat <iamerkanat@gmail.com>
2023-02-19 18:55:40 +0600

    h

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+var music = ["My Mind & Me", "Born To Die", "You're Somebody Else"];
+var artist = ['Selena Gomez', 'Flora Cash', 'Lana Del Rey'];
+
+console.log(music[0]+" by "+artist[0]);
+console.log(music[1]+" by "+artist[2]);
+console.log(music[2]+" by "+artist[1]);
+
+
+let artist1 ={firstName: "ERKANAT", lastName: "O", profession: "artist", we: 5};
+
+document.querySelector("#button").onclick = function(){
+
+    const button = document.querySelector("#button");
+    button.innerText = "SOLD!!!";
+}
+document.querySelector("#button1").onclick = function(){
+
+    const button = document.querySelector("#button1");
+    button.innerText = "SOLD!!!";
+}
+document.querySelector("#button2").onclick = function(){
+
+    const button = document.querySelector("#button2");
+    button.innerText = "SOLD!!!";
+}
+document.querySelector("#button3").onclick = function(){
+
+    const button = document.querySelector("#button3");
+    button.innerText = "SOLD!!!";
+}
+document.querySelector("#button4").onclick = function(){
+
+    const button = document.querySelector("#button4");
+    button.innerText = "SOLD!!!";
+}
+document.querySelector("#button5").onclick = function(){
+
+    const button = document.querySelector("#button5");
+    button.innerText = "SOLD!!!";
+}
